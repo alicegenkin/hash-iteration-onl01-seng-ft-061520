2f826b373117e2d80ced3f66b0bbb2967de3f079
Alice <agenkin1@gmail.com>
2020-06-27 02:55:20 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,6 +9,7 @@ def happy_birthday(birthday_kids)
       if age < 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   else puts "no"
+  end
     end
   # add your code snippet here!
 end
