d807d7de043cb7b378bfb0d919997438c2183772
Alice <agenkin1@gmail.com>
2020-06-27 03:02:56 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,6 +16,6 @@ def age_appropriate_birthday(birthday_kids)
   if age < 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
-    return "You are too old for this."
+    elsif return "You are too old for this."
 end
 end
